4f84b1b62af9b1ed1ec67c01dcc19049b1a49696
Rim Hassane <hassanerim17@gmail.com>
2025-12-22 16:56:24 +0200

    Deuxieme commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import emsiLogo from './assets/logoEmsi.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>MBDS</h1>
+    <header className="header">
+      <img src={emsiLogo} className="logo-emsi" alt="EMSI Logo" />
+      <h1>Introduction à React</h1>
+      <h2>A la découverte des premières notions de React</h2>
+    </header>
+  )
+}
+
+function Content() {
+  const [count, setCount] = useState(0)
+  return (
+    <main className="main-content">
+      Ici, nous afficherons des informations interessantes :)
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Edit <code>src/App.jsx</code> and save to test HMR</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      Tous droits réservés - Rim HASSANE
+    </footer>
+  )
+}
+
+function App() {
+  return (
+    <div id="root">
+      <Header />
+      <Content />
+      <Footer />
+    </div>
   )
 }
 
